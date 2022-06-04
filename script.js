e530702164a2e78774ef5634b4b9ec5c31939471
mashru020 <mashrurrahman16@gmail.com>
2022-06-04 23:21:08 +0600

    label data cnage functionality add

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 'use strict';
-// let section = document.querySelector('#home');
-
-// let label = section.id;
-
-// const displaylabel = function (label) {
-//     document.querySelector('.label__section p').textContent = label;
-//   };
 
   new fullpage('#fullpage', {
 	//options here
 	autoScrolling:true,
 	scrollHorizontally: true
 });
+
+// seltect elements
+let sectionLabel = document.querySelector('.label__section p');
+let paginationLabel = document.querySelector('.label__pagination h3');
+console.log(sectionLabel);
+let section =document.querySelector('.section.active');
+let sectionName = document.querySelector('.section.active').id;
+console.log(sectionName);
+let className= section.classList[1];
+console.log(className);
+let classNameLength = className.length;
+console.log(classNameLength);
+let sectionNumber = className.slice(classNameLength - 1);
+console.log(sectionNumber);
+sectionLabel.textContent = sectionName;
+paginationLabel.textContent = sectionNumber;
