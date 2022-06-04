745bbac510ee01a56f5d3338a334fe491d7a833a
mashru020 <mashrurrahman16@gmail.com>
2022-06-04 17:46:40 +0600

    full page scroll cdn added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+// let section = document.querySelector('#home');
+
+// let label = section.id;
+
+// const displaylabel = function (label) {
+//     document.querySelector('.label__section p').textContent = label;
+//   };
+
+  new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true
+});
