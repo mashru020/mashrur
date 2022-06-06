a655d4b7b265e7226a00df66f4c5f35c23006910
mashru020 <mashrurrahman16@gmail.com>
2022-06-06 21:02:46 +0600

    page number change

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const navButton = document.querySelector('.nav__btn');
 const navBar = document.querySelector('.nav__wrapper');
 const sectionLabel = document.querySelector('.label__section p');
 const paginationLabel = document.querySelector('.label__pagination h3');
-const section =document.querySelector('.section.active');
+let section = document.querySelector('.section.active');
 const sectionId = document.querySelector('.section.active').id;
 
 
@@ -31,12 +31,16 @@ document.addEventListener('click', function(e) {
     // var target = e.target || e.srcElement,
     //     text = target.textContent || target.innerText;  
 	if (e.target.classList.contains('nav__link')) {
-		console.log(e.target.classList);
+		let sectionLabelValue = e.target.innerHTML.toLowerCase();
+		sectionLabel.innerHTML = (sectionLabelValue) ;
+		console.log(e);
 		navButton.classList.remove('open');
 		navBar.classList.toggle('show');
 		navBar.classList.toggle('hide');
+
+
+		let pagValue = e.target.hash;
+		paginationLabel.innerHTML = (0+pagValue[10]);
 	}
 		 
 }, false);
-
-
