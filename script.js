c9464fae845493a14bd0feeea2ca5bcdf556323b
mashru020 <mashrurrahman16@gmail.com>
2022-06-06 10:39:25 +0600

    navbar js

--- script.js
+++ script.js
@@ -15,10 +15,28 @@ const section =document.querySelector('.section.active');
 const sectionId = document.querySelector('.section.active').id;
 
 
-console.log(navButton);
-navButton.addEventListener('click',function(){
+
+navButton.addEventListener('click',function(e){
 	navBar.classList.toggle('hide');
 	navBar.classList.toggle('show');
 	navButton.classList.toggle('open');
+	// console.log(e);
 });
+
+// const navLink = document.querySelector(".nav__link");
+// console.log(navLink);
+
+document.addEventListener('click', function(e) {
+    // e = e || window.event;
+    // var target = e.target || e.srcElement,
+    //     text = target.textContent || target.innerText;  
+	if (e.target.classList.contains('nav__link')) {
+		console.log(e.target.classList);
+		navButton.classList.remove('open');
+		navBar.classList.toggle('show');
+		navBar.classList.toggle('hide');
+	}
+		 
+}, false);
+
 
