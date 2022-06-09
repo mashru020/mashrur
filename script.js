218f246b9e054fb8ccdb59583dc228c326710569
mashru020 <mashrurrahman16@gmail.com>
2022-06-09 16:51:32 +0600

    label change on scroll

--- script.js
+++ script.js
@@ -20,11 +20,56 @@ const sectionId = document.querySelector('.section.active').id;
 const labelStyle = function (val1, val2, val3) {
 	let x = `${val1}px`;
 	let y = `${val2}px`;
-	console.log(x , y);
+	// console.log(x , y);
 	sectionLabelText.style.width = `${val3*7.5}px`
 	paginationLabel.style.transform = `translateY(${x})`;
 	sectionLabelText.style.transform = `translateY(${y})`;
 
+}
+
+
+// functions 
+
+// label change form nav item
+const changeLabelNav = (e) => {
+	sectionLabelValue = e.target.innerHTML.toLowerCase();
+	labelStyle(-50, -50,sectionLabelValue.length);
+	pagValue = e.target.hash;
+
+	setTimeout(() => {
+		sectionLabelText.innerHTML = (sectionLabelValue) ;
+		paginationLabel.innerHTML = (0+pagValue[10]);
+		labelStyle(0,0, sectionLabelValue.length);
+	}, 1000);
+}
+
+// label change with scroll
+const changeLabelScroll = (e) => {
+	const currentSection = document.querySelector('.section.active');
+	console.log(currentSection);
+	pagValue = currentSection.id[9];
+	
+	
+	
+	sectionLabelValue = currentSection.classList[1];
+	labelStyle(-50, -50,sectionLabelValue.length);
+	setTimeout(() => {
+		sectionLabelText.innerHTML = (sectionLabelValue) ;
+		paginationLabel.innerHTML = (0+pagValue);
+		labelStyle(0,0, sectionLabelValue.length);
+	}, 1000);
+}
+
+const changeLabelKey = (val) => {
+	if (val === 38 || val === 33) {
+		pagValue = `${Number(pagValue) + 1}`;
+	}
+	else if (val === 40 || val === 34) {
+		pagValue = `${Number(pagValue) - 1}`;
+	}
+	else {
+		console.log("press up or down arrow");
+	}
 }
 
 navButton.addEventListener('click',function(e){
@@ -35,29 +80,32 @@ navButton.addEventListener('click',function(e){
 document.addEventListener('click', function(e) {  
 	if (e.target.classList.contains('nav__link')) {
 		
-		sectionLabelValue = e.target.innerHTML.toLowerCase();
-		labelStyle(-50, -50,sectionLabelValue.length);
+		
 		// sectionLabel.innerHTML = (sectionLabelValue) ;
 		navButton.classList.remove('open');
 		navBar.classList.toggle('show');
 		navBar.classList.toggle('hide');
 
 
-		pagValue = e.target.hash;
+		changeLabelNav(e);
 		
-		console.log(sectionLabelValue.length);
-		console.log(sectionLabelValue);
-		console.log(sectionLabelText);
-		setTimeout(() => {
-			sectionLabelText.innerHTML = (sectionLabelValue) ;
-			paginationLabel.innerHTML = (0+pagValue[10]);
-			labelStyle(0,0, sectionLabelValue.length);
-		}, 1000);
+		// console.log(sectionLabelValue.length);
+		// console.log(sectionLabelValue);
+		// console.log(sectionLabelText);
+		
 		
 	}
 		 
 }, false);
 document.addEventListener('wheel',function(e) {
-	console.log(e);
+	changeLabelScroll(e);
 });
+
+// document.addEventListener('keydown',function(e) {
+// 	console.log(e.target);
+// 	// e.preventDefault();
+// 	// changeLabelKey(e.keyCode);
+	
+	
+// });
 
