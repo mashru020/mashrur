c6fb76bcc983fcd1f5128275b2b59716bceec201
mashru020 <mashrurrahman16@gmail.com>
2022-06-12 16:08:18 +0600

    fullpage menu study

--- script.js
+++ script.js
@@ -1,111 +1,90 @@
 'use strict';
 
-  new fullpage('#fullpage', {
-	//options here
-	autoScrolling:true,
-	scrollHorizontally: true
+var myFullpage = new fullpage('#fullpage', {
+
+	
+	// Navigation
+	menu: '#menu',
+	lockAnchors: false,
+	anchors:['home', 'skills','services', 'profile'],
+	navigation: false,
+	navigationPosition: 'right',
+	navigationTooltips: ['firstSlide', 'secondSlide'],
+	showActiveTooltip: false,
+	slidesNavigation: false,
+	slidesNavPosition: 'bottom',
+
+	// Scrolling
+	css3: true,
+	scrollingSpeed: 700,
+	autoScrolling: true,
+	fitToSection: true,
+	scrollBar: false,
+	easing: 'easeInOutCubic',
+	easingcss3: 'ease',
+	loopBottom: false,
+	loopTop: false,
+	loopHorizontal: true,
+	continuousVertical: false,
+	continuousHorizontal: false,
+	scrollHorizontally: false,
+	interlockedSlides: false,
+	dragAndMove: false,
+	offsetSections: false,
+	resetSliders: false,
+	fadingEffect: false,
+	normalScrollElements: '#element1, .element2',
+	scrollOverflow: true,
+	scrollOverflowMacStyle: false,
+	scrollOverflowReset: false,
+	touchSensitivity: 15,
+	bigSectionsDestination: null,
+
+	// Accessibility
+	keyboardScrolling: true,
+	animateAnchor: true,
+	recordHistory: true,
+
+	// Design
+	controlArrows: true,
+	controlArrowsHTML: [
+		'<div class="fp-arrow"></div>', 
+		'<div class="fp-arrow"></div>'
+	],
+	verticalCentered: true,
+	// sectionsColor : ['#ccc', '#fff'],
+	paddingTop: '3em',
+	paddingBottom: '10px',
+	fixedElements: '#header, .footer',
+	responsiveWidth: 0,
+	responsiveHeight: 0,
+	responsiveSlides: false,
+	parallax: false,
+	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+	dropEffect: false,
+	dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
+	waterEffect: false,
+	waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
+	cards: false,
+	cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+
+	// Custom selectors
+	sectionSelector: '.section',
+	slideSelector: '.slide',
+
+	lazyLoading: true,
+	observer: true,
+	credits: { enabled: true, label: 'Made with fullPage.js', position: 'right'},
+
+	// Events
+	beforeLeave: function(origin, destination, direction, trigger){},
+	onLeave: function(origin, destination, direction, trigger){},
+	afterLoad: function(origin, destination, direction, trigger){},
+	afterRender: function(){},
+	afterResize: function(width, height){},
+	afterReBuild: function(){},
+	afterResponsive: function(isResponsive){},
+	afterSlideLoad: function(section, origin, destination, direction, trigger){},
+	onSlideLeave: function(section, origin, destination, direction, trigger){},
+	onScrollOverflow: function(section, slide, position, direction){}
 });
-
-// seltect elements
-let sectionLabelValue = "";
-let pagValue;
-const navButton = document.querySelector('.nav__btn');
-const navBar = document.querySelector('.nav__wrapper');
-const sectionLabel = document.querySelector('.label__section');
-const sectionLabelText = document.querySelector('.label__section p');
-const paginationLabel = document.querySelector('.label__pagination h3');
-let section = document.querySelector('.section.active');
-const sectionId = document.querySelector('.section.active').id;
-
-const labelStyle = function (val1, val2, val3) {
-	let x = `${val1}px`;
-	let y = `${val2}px`;
-	// console.log(x , y);
-	sectionLabelText.style.width = `${val3*8}px`
-	paginationLabel.style.transform = `translateY(${x})`;
-	sectionLabelText.style.transform = `translateY(${y})`;
-
-}
-
-
-// functions 
-
-// label change form nav item
-const changeLabelNav = (e) => {
-	sectionLabelValue = e.target.innerHTML.toLowerCase();
-	labelStyle(-50, -50,sectionLabelValue.length);
-	pagValue = e.target.hash;
-
-	setTimeout(() => {
-		sectionLabelText.innerHTML = (sectionLabelValue) ;
-		paginationLabel.innerHTML = (0+pagValue[10]);
-		labelStyle(0,0, sectionLabelValue.length);
-	}, 1000);
-}
-
-// label change with scroll
-const changeLabelScroll = (e) => {
-	const currentSection = document.querySelector('.section.active');
-	console.log(currentSection);
-	pagValue = currentSection.id[9];
-	
-	
-	
-	sectionLabelValue = currentSection.classList[1];
-	labelStyle(-50, -50,sectionLabelValue.length);
-	setTimeout(() => {
-		sectionLabelText.innerHTML = (sectionLabelValue) ;
-		paginationLabel.innerHTML = (0+pagValue);
-		labelStyle(0,0, sectionLabelValue.length);
-	}, 1000);
-}
-
-const changeLabelKey = (val) => {
-	if (val === 38 || val === 33) {
-		pagValue = `${Number(pagValue) + 1}`;
-	}
-	else if (val === 40 || val === 34) {
-		pagValue = `${Number(pagValue) - 1}`;
-	}
-	else {
-		console.log("press up or down arrow");
-	}
-}
-
-navButton.addEventListener('click',function(e){
-	navBar.classList.toggle('hide');
-	navBar.classList.toggle('show');
-	navButton.classList.toggle('open');
-});
-document.addEventListener('click', function(e) {  
-	if (e.target.classList.contains('nav__link')) {
-		
-		
-		// sectionLabel.innerHTML = (sectionLabelValue) ;
-		navButton.classList.remove('open');
-		navBar.classList.toggle('show');
-		navBar.classList.toggle('hide');
-
-
-		changeLabelNav(e);
-		
-		// console.log(sectionLabelValue.length);
-		// console.log(sectionLabelValue);
-		// console.log(sectionLabelText);
-		
-		
-	}
-		 
-}, false);
-document.addEventListener('wheel',function(e) {
-	changeLabelScroll(e);
-});
-
-// document.addEventListener('keydown',function(e) {
-// 	console.log(e.target);
-// 	// e.preventDefault();
-// 	// changeLabelKey(e.keyCode);
-	
-	
-// });
-
