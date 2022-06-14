af62b64e650834da416bc0c5e03c111b6a470b62
mashru020 <mashrurrahman16@gmail.com>
2022-06-14 23:28:08 +0600

    js update as dev

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
   new fullpage('#fullpage', {
 	//options here
+	menu: '#menu-list',
+	anchors:['home', 'skills', 'services', 'profile'],
+	navigation: true,
+	navigationPosition: 'left',
 	autoScrolling:true,
-	scrollHorizontally: true,
+	scrollHorizontally: true
 });
-
 
 // seltect elements
 
@@ -24,10 +27,6 @@ let pagValue;
 const length = allSection.length;
 
 // functions 
-
-const getCurrentSection= () => {
-	return document.querySelector('.section.active');
-}
 const labelStyle = function (val1, val2, val3) {
 	let x = `${val1}px`;
 	let y = `${val2}px`;
@@ -35,44 +34,19 @@ const labelStyle = function (val1, val2, val3) {
 	sectionLabelText.style.width = `${val3*8}px`
 	paginationLabel.style.transform = `translateY(${x})`;
 	sectionLabelText.style.transform = `translateY(${y})`;
-
 }
 
-
- 
-//check last and first section 
-const checkFirstLastSection = () => {
-	const currentSectionTest = getCurrentSection().id;
-	console.log(currentSectionTest);
-	const firstSection = allSection[0].id;
-	const lastSection = allSection[length - 1].id;
-	
-	if (firstSection == currentSectionTest || lastSection == currentSectionTest ){
-		return 0;
-	}
-	else return 1;
-}
-// ========================label change form nav item
-const changeLabelNav = (e) => {
-	sectionLabelValue = e.target.innerHTML.toLowerCase();
-	labelStyle(-50, -50,sectionLabelValue.length);
-	pagValue = e.target.hash;
-
-	setTimeout(() => {
-		sectionLabelText.innerHTML = (sectionLabelValue) ;
-		paginationLabel.innerHTML = (0+pagValue[10]);
-		labelStyle(0,0, sectionLabelValue.length);
-	}, 1000);
+// return active section
+const getCurrentSection= () => {
+	return document.querySelector('.section.active');
 }
 
 // ==============  label change with scroll
-const changeLabel = (e) => {
+const changeLabel = () => {
 	const currentSection = getCurrentSection();
-	pagValue = currentSection.id[9];
-	
-	
-	
-	sectionLabelValue = currentSection.classList[1];
+	pagValue = currentSection.classList[1][9];
+	sectionLabelValue = currentSection.dataset.anchor;
+
 	labelStyle(-50, -50,sectionLabelValue.length);
 	setTimeout(() => {
 		sectionLabelText.innerHTML = (sectionLabelValue) ;
@@ -81,10 +55,6 @@ const changeLabel = (e) => {
 	}, 1000);
 }
 
-
-
-
-
 // EVENTS
 navButton.addEventListener('click',function(e){
 	navBar.classList.toggle('hide');
@@ -92,31 +62,25 @@ navButton.addEventListener('click',function(e){
 	navButton.classList.toggle('open');
 });
 
-
-
 document.addEventListener('click', function(e) {  
 	if (e.target.classList.contains('nav__link')) {
-		
-		
-		// sectionLabel.innerHTML = (sectionLabelValue) ;
 		navButton.classList.remove('open');
 		navBar.classList.toggle('show');
 		navBar.classList.toggle('hide');
-
-		changeLabelNav(e);
-	}
-		 
+		setTimeout(() => {
+			changeLabel();
+		}, 1);	
+	}	 
 }, false);
+
 document.addEventListener('wheel',function(e) {
-	changeLabel(e);
-	console.log(checkFirstLastSection());
+	changeLabel();
 });
 
 document.addEventListener('keydown',function(e) {
-	// console.log(e.target);
-	e.preventDefault();
-	changeLabel(e);
-	console.log(checkFirstLastSection());
-	
+	console.log(e);
+	setTimeout(() => {
+		changeLabel();
+	}, 1);
 });
 
